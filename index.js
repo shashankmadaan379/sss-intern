8e20b70d361096c74efcfa3cea9e19c4eb33bbe8
shashankmadaan379 <codebreaker379@gmail.com>
2023-07-05 23:16:51 +0530

    added cart section css pending

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var express = require("express");
 var ejs = require("ejs");
-
+var bodyParser = require("body-parser");
 var app = express();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.listen(8080);
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", function (req, res) {
   res.render("pages/index");
 });
